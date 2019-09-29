1a9a4db1c4dd9e7f375366bd91d8b0ca9dba2afa
szydlinho <pszydlik@gmail.com>
2019-09-29 20:36:26 +0200

    Modyfikacja skryptu pod tworzenia podstawowego datasetu

--- functions/dataset_preparation.py
+++ functions/dataset_preparation.py
@@ -11,11 +11,11 @@ import numpy as np
 import pandas as pd
 from datetime import datetime as dt
 #import itertools
-import progressbar
+#import progressbar
 import datetime
 import requests
 pd.options.mode.chained_assignment = None
-pbar = progressbar.ProgressBar().start()
+#pbar = progressbar.ProgressBar().start()
 from datetime import timedelta  
 import os, re
 discks_names =  re.findall(r"[A-Z]+:.*$",os.popen("mountvol /").read(),re.MULTILINE)
@@ -26,7 +26,7 @@ for i in range(len(discks_names)):
     a.append(b)
 
 dics = a['C' in a]
-dics = a[0]
+dics = a[2]
 #dics = 'D'    
 
 
@@ -45,12 +45,22 @@ dics = a[0]
 
 abb = ['E0', 'E1', 'D1', 'D2', 'I1', 'I2', 'SP1','SP2','F1', 'F2', 'N1', 'P1']
 
-
+league_abb = 'E1'
 #
 
 
 league = 'germany/2-bundesliga'
-def PrepareDataset(league_abb, league):
+league_url = 'germany/2_bundesliga/'
+league_url = 'england/premier_league/'
+league_url = 'england/championship/'
+league_url = 'germany/1_bundesliga/'
+league_url = 'italy/serie_a/'
+league_url = 'italy/serie_b_italy_football/'
+league_url = 'france/ligue_1/'
+league_url = 'france/ligue_2/'
+league_url = 'netherlands/eredivisie/'
+league_url = 'portugal/primeira_liga/'
+def PrepareDataset(league_abb, league, data_uk = False):
     
     loc = dics + ":/data_football/final_datasets/"
     
@@ -62,8 +72,17 @@ def PrepareDataset(league_abb, league):
     
     dataset = {}
     # 10strings: `raw_data_10, .. ,raw_data_19`
-    dataset_names = ["raw_data_%d" % x for x in range(10, 21)]
-    sezony = ["09-10", "10-11", "11-12", "12-13", "13-14","14-15", "15-16",
+    
+    
+    if league_abb == 'P1':
+        dataset_names = ["raw_data_%d" % x for x in range(15, 21)]
+    else:
+        dataset_names = ["raw_data_%d" % x for x in range(10, 21)]
+ 
+    if league_abb == 'P1':
+        sezony = ["14-15", "15-16", "16-17", "17-18", "18-19", "19-20"]
+    else:
+        sezony = ["09-10", "10-11", "11-12", "12-13", "13-14","14-15", "15-16",
               "16-17", "17-18", "18-19", "19-20"]
     
     for (data_set, sezon) in zip(dataset_names, sezony):
@@ -79,22 +98,31 @@ def PrepareDataset(league_abb, league):
     a.HomeTeam.unique()
     pd.Series(a['HomeTeam'].unique()).sort_values()
     for data_set in dataset_names:
+        dataset[data_set]= dataset[data_set].dropna(thresh=2) 
         dataset[data_set].Date = pd.to_datetime(dataset[data_set].Date)
     
 
-    if (len(dataset["raw_data_10"])% 10 == 0):
+    if(len(dataset["raw_data_15"])% 8 == 0):
+        number_of_MW = 8
+    elif (len(dataset["raw_data_15"])% 10 == 0):
         number_of_MW = 10
-    elif(len(dataset["raw_data_10"])% 9 == 0):
+    elif(len(dataset["raw_data_15"])% 9 == 0):
         number_of_MW = 9
-    elif(len(dataset["raw_data_10"])% 12 == 0):
+    elif(len(dataset["raw_data_15"])% 12 == 0):
         number_of_MW = 12
+    elif(len(dataset["raw_data_15"])% 11 == 0):
+        number_of_MW = 11
         
     for data_set in dataset_names:
         f1.fill_with_mw( dataset[data_set], number_of_MW)
         
     #Wybor tylko tych kolumn, ktore potrzebuje
     columns_req = ['Date','HomeTeam','AwayTeam','FTHG','FTAG','FTR', 'MW']
-    dataset_names_mew = ["%d" % x for x in range(10, 21)]
+    if league_abb == 'P1':
+        dataset_names_mew =  ["%d" % x for x in range(15, 21)]
+    else:
+        dataset_names_mew = ["%d" % x for x in range(10, 21)]
+        
     playing_stat = {}
     
     for (data_set, datase_old) in zip(dataset_names_mew, dataset_names):
@@ -102,25 +130,30 @@ def PrepareDataset(league_abb, league):
          
     for data_set in dataset_names_mew:
          playing_stat[data_set] =  playing_stat[data_set].dropna()
+    
 
-         
+    #playing_stat['10'] = playing_stat['10'].replace('Piacenza ', 'Piacenza')
+    
+    for data_set in dataset_names_mew:
+        playing_stat[data_set]['HomeTeam'] = playing_stat[data_set]['HomeTeam'].str.strip()
+        playing_stat[data_set]['AwayTeam'] = playing_stat[data_set]['AwayTeam'].str.strip()
     #Suma goli w sezonie dla kazdej z druzyn oraz numer kolejki
     #bramki
     for data_set in dataset_names_mew:
-         playing_stat[data_set] = f1.get_gss(playing_stat[data_set], number_of_MW)
+        playing_stat[data_set] = f1.get_gss(playing_stat[data_set], number_of_MW)
     
     #PUNKTACJA###############################################
     for data_set in dataset_names_mew:
-         playing_stat[data_set] = f1.get_agg_points(playing_stat[data_set], number_of_MW)
+        playing_stat[data_set] = f1.get_agg_points(playing_stat[data_set], number_of_MW)
 
     for data_set in dataset_names_mew:
-         playing_stat[data_set] = f1.get_agg_wins(playing_stat[data_set], number_of_MW)
+        playing_stat[data_set] = f1.get_agg_wins(playing_stat[data_set], number_of_MW)
          
     for data_set in dataset_names_mew:
-         playing_stat[data_set] = f1.get_agg_losses(playing_stat[data_set], number_of_MW)
+        playing_stat[data_set] = f1.get_agg_losses(playing_stat[data_set], number_of_MW)
 
     for data_set in dataset_names_mew:
-         playing_stat[data_set] = f1.get_agg_draws(playing_stat[data_set], number_of_MW)
+        playing_stat[data_set] = f1.get_agg_draws(playing_stat[data_set], number_of_MW)
 
     
     #####FORMA DRUZYNY ##################################
@@ -213,25 +246,38 @@ def PrepareDataset(league_abb, league):
     #New fixtures
     #league = 'england/premier-league'
     
-    url = 'https://us.soccerway.com/national/'+ league + '/20192020/regular-season/'
-    response = requests.get(url)
-    tables = pd.read_html(response.text)
-    table = tables[0]
+    #url = 'https://us.soccerway.com/national/'+ league + '/20192020/regular-season/'
+    #response = requests.get(url)
+    #tables = pd.read_html(response.text)
+    #table = tables[0]
     #table[table["Score/Time"]]
-    fixtures =     pd.read_csv("https://www.football-data.co.uk/fixtures.csv")
-    fixtures = fixtures[fixtures["Div"] == league_abb]
-    #fixtures = table[["Date", "Home team", "Away team"]]
-    fixtures = fixtures[["Date", "HomeTeam", "AwayTeam"]]
+    if  data_uk == True: 
+        fixtures =     pd.read_csv("https://www.football-data.co.uk/fixtures.csv")
+        fixtures = fixtures[fixtures["Div"] == league_abb]
+        fixtures = fixtures[["Date", "HomeTeam", "AwayTeam"]]
+    else:
+        url = 'http://www.tablesleague.com/' + league_url
+        response = requests.get(url)
+        tables = pd.read_html(response.text)
+        table = tables[1]
+        table.columns = table.iloc[0]
+        table = table.drop(table.index[0])
+        fixtures = pd.concat([table["Date"], table["Game"].str.split(" - ", expand= True)], axis=1)
+        fixtures.Date = fixtures.Date.str.replace("Today", datetime.date.today().strftime("%d %b"))
+        fixtures.Date = pd.to_datetime(fixtures.Date ,  format = "%d %b %H:%M")
+        fixtures.columns = ['Date', 'HomeTeam', 'AwayTeam']
+        
+        for i in range(0, len(fixtures)):
+            fixtures["HomeTeam"].iloc[i] = fixtures["HomeTeam"].iloc[i].replace(fixtures["HomeTeam"].iloc[i], names_dict2[fixtures["HomeTeam"].iloc[i]])
+        
+        for i in range(0, len(fixtures)):
+            fixtures["AwayTeam"].iloc[i] = fixtures["AwayTeam"].iloc[i].replace(fixtures["AwayTeam"].iloc[i], names_dict2[fixtures["AwayTeam"].iloc[i]])
+        
     #fixtures.columns = ['Date', 'HomeTeam', 'AwayTeam']
     #fixtures = fixtures.dropna(how='all', axis=0)
     #fixtures = fixtures[pd.notnull(fixtures['HomeTeam'])]
     #Tu trzeba jakos zrobic aby dzielił mecze na MW, bo na stronie jest wiecej niz z jednej kolejki
-    #for i in range(0, len(fixtures)):
-    #    fixtures["HomeTeam"].iloc[i] = fixtures["HomeTeam"].iloc[i].replace(fixtures["HomeTeam"].iloc[i], names_dict2[fixtures["HomeTeam"].iloc[i]])
-        
-    #for i in range(0, len(fixtures)):
-    #    fixtures["AwayTeam"].iloc[i] = fixtures["AwayTeam"].iloc[i].replace(fixtures["AwayTeam"].iloc[i], names_dict2[fixtures["AwayTeam"].iloc[i]])
-        
+
     fixtures["MW"] = 0
     if dataset_final.iloc[len(dataset_final)-1]["MW"] == ((number_of_MW * 4)-2):
         fixtures["MW"][0:number_of_MW] = 1
@@ -239,8 +285,11 @@ def PrepareDataset(league_abb, league):
     else:
         fixtures["MW"][0:number_of_MW] = dataset_final.iloc[len(dataset_final)-1]["MW"] + 1
         fixtures["MW"][number_of_MW:] = dataset_final.iloc[len(dataset_final)-1]["MW"] + 2
-        
+    
     fixtures.Date = pd.to_datetime(fixtures.Date)
+    for i in range(0, len(fixtures)):
+        fixtures.Date.iloc[i] = fixtures.Date.iloc[i].replace(year = 2019)
+
     fixtures["Sezon"] = 20
     #fixtures["MW"] = int(max(playing_statistics_19['MW'])+1)
     if fixtures.iloc[len(fixtures)-1]["MW"] != 1:
@@ -366,11 +415,11 @@ def PrepareDataset(league_abb, league):
     names_elo.columns = ["old", "new"]
     names_dict_elo = pd.Series(names_elo.new.values,index=names_elo.old).to_dict()
     
-     temp = final.loc[final['MW']==1]
-     final["Elo_HT"] = 0
-     final["Elo_AT"] = 0
+    temp = final.loc[final['MW']==1]
+    final["Elo_HT"] = 0
+    final["Elo_AT"] = 0
      
-     if len(temp) % number_of_MW == 0:
+    if len(temp) % number_of_MW == 0:
          for i in range(0, len(temp), number_of_MW):
              #.strftime("%Y-%m-%d")
              date = str(str(temp["Date"].iloc[i].year) + '-09-20')
@@ -390,16 +439,16 @@ def PrepareDataset(league_abb, league):
      
       
     final.to_csv(loc + league_abb + "_log.csv")
-    to_predict = final.loc[final['Sezon']==20]
+    #to_predict = final.loc[final['Sezon']==20]
     
     #if (len(fixtures) % number_of_MW) == 0:
     #    to_predict = fixtures
     #else:
-    to_predict = to_predict.loc[to_predict["MW"]== int(max(playing_stat['20']['MW'])+1)]
+    #to_predict = to_predict.loc[to_predict["MW"]== int(max(playing_stat['20']['MW'])+1)]
     
     
-    to_predict.to_csv(loc + "predict_"+league_abb+".csv", index=False)
-    print(league_abb + " wrzucone")
+    #to_predict.to_csv(loc + "predict_"+league_abb+".csv", index=False)
+    #print(league_abb + " wrzucone")
      
         
      # dataset_final = pd.concat(playing_stat, sort=False)
@@ -407,25 +456,25 @@ def PrepareDataset(league_abb, league):
 #         playing_stat[data_set]["Sezon"] = int(data_set)
 #     
 #     dataset_final = pd.concat(playing_stat, sort=False)
-#     temp = dataset_final.loc[dataset_final['MW']==1]
-#   
-#     elo_teams =[]
-#     for i in range(0, len(temp)+1, number_of_MW):
-#             date = str(temp["Date"].iloc[i].strftime("%Y-%m-%d"))
-#             sezon = temp["Sezon"].iloc[i]
-#             elo = pd.read_csv('http://api.clubelo.com/' + date)
-#             elo_new_teams = elo.loc[elo["Country"] == "GER"]["Club"].unique().tolist()   
-#             elo_teams.extend(elo_new_teams)
-#     
-#     dataset_final["HomeTeam"].unique().tolist()
-#     elo_teams.unique().tolist()
-#     list(set(elo_teams))
+     #temp = dataset_final.loc[dataset_final['MW']==1]
+   
+     #elo_teams =[]
+     #for i in range(0, len(temp)+1, number_of_MW):
+     #        date = str(temp["Date"].iloc[i].strftime("%Y-%m-%d"))
+     #        sezon = temp["Sezon"].iloc[i]
+     #        elo = pd.read_csv('http://api.clubelo.com/' + date)
+     #        elo_new_teams = elo.loc[elo["Country"] == "NED"]["Club"].unique().tolist()   
+     #        elo_teams.extend(elo_new_teams)
+     
+     #dataset_final["HomeTeam"].unique().tolist()
+     #elo_teams.unique().tolist()
+     #sorted(list(set(elo_teams)))
      
      #elo = pd.read_csv('http://api.clubelo.com/2019-06-01')
      #elo = pd.read_csv('http://api.clubelo.com/Arsenal')
      #new_fix = pd.read_csv('http://api.clubelo.com/Fixtures')
      
-     cols_final = ["Date", "HomeTeam", "AwayTeam", "FTR", "HTGS", "ATGS", "HTGC", 
+    cols_final = ["Date", "HomeTeam", "AwayTeam", "FTR", "HTGS", "ATGS", "HTGC", 
                    "ATGC", "HTP", "ATP",  "HT_Wins", "AT_Wins", "HT_Loss", 
                    "AT_Loss", "HT_Draws", "AT_Draws", "MW", "HomeTeamLP", "AwayTeamLP", "Sezon",
                    "HT_LP", "AT_LP", "HTFormPts", "ATFormPts", "HTWinStreak5", 
@@ -436,47 +485,76 @@ def PrepareDataset(league_abb, league):
                    'H2H_Home_pts', 'H2H_Away_pts', 'Off_A', 'Deff_H', 'Deff_A', 'Mean_home_goals', 'Mean_away_goals',
                    'Elo_HT', 'Elo_AT']
      
-     final_df = final[cols_final]
+    final_df = final[cols_final]
      
-     cols_to_diff = [ "HTGS", "ATGS", "HTGC", 
+    cols_to_diff = [ "HTGS", "ATGS", "HTGC", 
                    "ATGC", "HTP", "ATP",  "HT_Wins", "AT_Wins", "HT_Loss", 
                    "AT_Loss", "HT_Draws", "AT_Draws", 'HTGD', 'ATGD']
      
      
-     for col in cols_to_diff:
+    for col in cols_to_diff:
         final_df[col] =  final_df[col] / final_df["MW"]
      
-     marketv = pd.read_csv(loc + 'budget_'+ league_abb +'_final.csv', index_col = 0)
+    marketv = pd.read_csv(loc + 'budget_'+ league_abb +'_final.csv', index_col = 0,  encoding='latin-1')
     
-     marketv.Sezon = marketv.Sezon.astype(str).str[2:]
-     marketv["Sezon"] = marketv["Sezon"].astype(int) + 1
+    marketv.Sezon = marketv.Sezon.astype(str).str[2:]
+    marketv["Sezon"] = marketv["Sezon"].astype(int) + 1
     
-     dataset = pd.merge(final_df, marketv,  how='inner', left_on=['HomeTeam','Sezon'], 
+    dataset = pd.merge(final_df, marketv,  how='inner', left_on=['HomeTeam','Sezon'], 
                            right_on = ['Club','Sezon'], sort=True)
     
-     dataset = dataset.rename(columns={'Age': 'Age_H', 'Foreign': 'Foreign_H',
+    dataset = dataset.rename(columns={'Age': 'Age_H', 'Foreign': 'Foreign_H',
                                  'Total_value': 'Total_value_H', 'Market_value': 'Market_value_H'})
     
-     dataset = pd.merge(final_df, marketv,  how='inner', left_on=['AwayTeam','Sezon'], 
+    dataset = pd.merge(dataset, marketv,  how='inner', left_on=['AwayTeam','Sezon'], 
                            right_on = ['Club','Sezon'], sort=False)
     
-     dataset = dataset.rename(columns={'Age': 'Age_A', 'Foreign': 'Foreign_A',
+    dataset = dataset.rename(columns={'Age': 'Age_A', 'Foreign': 'Foreign_A',
                                     'Total_value': 'Total_value_A', 'Market_value': 'Market_value_A'})
         
-     df_final = dataset
-     df_final = df_final.drop_duplicates()
+    df_final = dataset
+    df_final = df_final.drop_duplicates()
      
-     to_predict = df_final.loc[df_final['Sezon']==20]
-     training_dataset = df_final.loc[df_final['Sezon']!=20]
+    #to_predict = df_final.loc[df_final['Sezon']==20]
+    #training_dataset = df_final.loc[df_final['Sezon']!=20]
      
-     to_predict = df_final.loc[(df_final['Sezon']==20) & (df_final['MW']==4)]
-     training_dataset =  df_final.loc[~((df_final['Sezon']==20) & (df_final['MW']==4))]
+    to_predict = df_final.loc[(df_final['Sezon']==20) & (df_final['MW']==int(max(playing_stat['20']['MW']))+1)]
+    training_dataset =  df_final.loc[~((df_final['Sezon']==20) & (df_final['MW']==int(max(playing_stat['20']['MW'])))+1)]
      
     
 
   
          
-     to_predict.to_csv(loc + "predict_"+league_abb+".csv", index=False)
-     training_dataset.to_csv(loc + "training_dataset_"+league_abb+".csv", index=False)
+    to_predict.to_csv(loc + "predict_"+league_abb+".csv", index=False)
+    training_dataset.to_csv(loc + "training_dataset_"+league_abb+".csv", index=False)
     
      
+
+
+PrepareDataset('E1','england/championship/',  data_uk = True )
+abb = ['E0', 'E1', 'D1', 'D2', 'I1', 'I2', 'SP1','SP2','F1', 'F2', 'N1', 'P1']
+
+league_abb = 'E1'
+#
+
+
+league = 'germany/2-bundesliga'
+league_url = 'germany/2_bundesliga/'
+league_url = 'england/premier_league/'
+league_url = 'england/championship/'
+league_url = 'germany/1_bundesliga/'
+league_url = 'italy/serie_a/'
+league_url = 'italy/serie_b_italy_football/'
+league_url = 'france/ligue_1/'
+league_url = 'france/ligue_2/'
+league_url = 'netherlands/eredivisie/'
+league_url = 'portugal/primeira_liga/'
+
+PrepareDataset('D1','germany/1_bundesliga/',  data_uk = True )
+PrepareDataset('D2','germany/2-bundesliga',  data_uk = True )
+PrepareDataset('I1','italy/serie_a/',  data_uk = True )
+PrepareDataset('I2','italy/serie_b/',  data_uk = True )
+PrepareDataset('F1','france/ligue_1/',  data_uk = True )
+PrepareDataset('F2','france/ligue_2/',  data_uk = True )
+PrepareDataset('N1','netherlands/eredivisie/',  data_uk = True )
+PrepareDataset('P1','portugal/primeira_liga/',  data_uk = True )
